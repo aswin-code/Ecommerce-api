b102c145963432c82b8fdaff6d5d857087580e08
aswin-code <aswinpops1234@gmail.com>
2022-11-17 15:45:35 +0530

    bug fixed

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const productController = require('../controller/productController')
-
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/products");
+    },
+    filename: (req, file, cb) => {
+        const ext = file.mimetype.split("/")[1];
+        cb(null, `${file.fieldname}-${Date.now()}.${ext}`);
+    },
+})
+const upload = multer({ storage })
 
 router.route('/')
     .get(productController.getAllProducts)
-    .post(productController.createProducts)
+    .post(upload.array('images', 3), productController.createProducts)
 // router.route('/:id')
 //     .get(userController.getAuser)
 //     .patch(userController.updateUser)
